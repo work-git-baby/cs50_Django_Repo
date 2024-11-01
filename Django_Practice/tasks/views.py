60861935fb46a1f210fec594324c3dda601dd99f
Godwin Kamau <godwin.kamau@jfmh.org>
2024-11-01 14:20:05 -0400

    Fixing up forms and learning about sessions

--- Django_Practice/tasks/views.py
+++ Django_Practice/tasks/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+'''
+This was the original way to create the task list(before talking about sessions)
 tasks = ['foo','bar','baz']
+'''
 
 class NewTaskForm(forms.Form):
-    tasks = forms.CharField(label="New Task")
+    task = forms.CharField(label="New Task")
     '''
     if you want to make a priority field-
     priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
@@ -12,16 +17,20 @@ class NewTaskForm(forms.Form):
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request, "tasks/index.html", {
-        'tasks': tasks
+        'tasks': request.session["tasks"]
     })
 
 def add(request):
     if request.method == "POST":
-        form = NewTaskForm(request,POST)
+        form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
                 "form": form
